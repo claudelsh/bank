fec103a5640ec98d455cdec27d3ce3def0f23d50
user <user@192.168.10.37>
2016-02-19 16:08:00 +0900

    `16.2.19 Oracle Test 성공

---
+++ bank/src/main/java/com/bank/web/util/Constants.java
@@ -0,0 +1,8 @@
+package com.bank.web.util;
+
+public class Constants {
+	public static final String ORACLE_DRIVER = "oracle.jdbc.OracleDriver";
+	public static final String ORACLE_URI = "jdbc:oracle:thin:@localhost:1521:xe";
+	public static final String ORACLE_ID = "bank";
+	public static final String ORACLE_PASS = "oracle";
+}
